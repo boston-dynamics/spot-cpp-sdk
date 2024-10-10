9a661601cea451635595bb836e4eff5fd7a46893
Andrew Messing <amessing@theaiinstitute.com>
2024-10-10 18:19:39 +0000

    Working towards depending on apt packages

---
+++ .devcontainer/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -v ~/spot-cpp-sdk:/spot-cpp-sdk spot_builder /entrypoint.sh
---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+cd /spot-cpp-sdk/cpp
+if [ -d build ]; then
+  rm -r build
+fi
+mkdir build
+cd build
+cmake .. -DCMAKE_FIND_PACKAGE_PREFER_CONFIG=TRUE -DBUILD_CHOREOGRAPHY_LIBS=ON
+make -j8 install package
