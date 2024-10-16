0a17fecf68df72066a5e2a7103d568d6d50f71ae
Andrew Messing <amessing@theaiinstitute.com>
2024-10-16 13:41:14 +0000

    Update arm version of the docker image

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
+ARCH=$(dpkg --print-architecture)
+
+if [ "$ARCH" == "amd64" ]; then
+  docker build -t spot_builder -f Dockerfile.amd64 .
+elif [ "$ARCH" == "arm64" ]; then
+  docker build -t spot_builder -f Dockerfile.arm64 .
+else
+  echo "Unknown architecture: $ARCH" > /dev/stderr
+  exit 1
+fi
+
 docker run -v ~/spot-cpp-sdk:/spot-cpp-sdk spot_builder /entrypoint.sh
